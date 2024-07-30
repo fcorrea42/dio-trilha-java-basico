29d324aa7cb06f64984a9030091ebbb10b979880
fcorrea42 <fcorreafelipe@gmail.com>
2024-07-30 00:20:29 -0300

    fechamento dos exemplos de tipos e variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // conhecer os tipos primitivos
+        // conhecer a classe String, que representa os textos na aplicação
+
+        String nomeCurso = "Java Básico";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
