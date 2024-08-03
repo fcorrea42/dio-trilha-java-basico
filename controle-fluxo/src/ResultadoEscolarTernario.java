96790bc1f96fa587eae8a3acb53e6a154e3e6032
fcorrea42 <fcorreafelipe@gmail.com>
2024-08-02 23:00:30 -0300

    Exemplos de Condicionais Ternárias

---
+++ controle-fluxo/src/ResultadoEscolarTernario.java
@@ -0,0 +1,11 @@
+public class ResultadoEscolarTernario {
+    public static void main(String[] args) {
+        int nota = 4;
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+
+        nota = 6;
+		resultado = nota >=7 ? "Aprovado" : nota >= 5 ? "Prova de Recuperação" : "Reprovado";
+		System.out.println(resultado);
+    }
+}
