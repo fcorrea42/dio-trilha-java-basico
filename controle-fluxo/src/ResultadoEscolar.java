fc328174707086a0b1f83fe086bdb1e30fab3bb3
fcorrea42 <fcorreafelipe@gmail.com>
2024-08-02 22:54:06 -0300

    Exemplos de Condicionais Encadeadas

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,9 +1,11 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 6;
+        int nota = 4;
         
         if(nota >= 7)
             System.out.println("Aprovado");
+        else if (nota >= 5)
+            System.out.println("Prova de Recuperação");
         else
             System.out.println("Reprovado");
     }
